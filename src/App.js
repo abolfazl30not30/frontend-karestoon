95847ad2c50f72d9b8256b93df994e364ee5b7ca
Abolfazl <abolfazl30not30@gmail.com>
2023-02-03 13:37:02 +0330

    merge sign up page

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import MainPage from "./Components/Home/MainPage/MainPage";
 import {BrowserRouter, Route,Routes} from "react-router-dom";
 import DashboardUser from "./Components/Dashboard User/Template/master"
 import DashboardAdmin from "./Components/Dashboard Admin/Template/master"
+import Login from "./Components/Signup/Login";
+import Signup from "./Components/Signup/Signup";
 
 function App() {
   return (
     <>
         <BrowserRouter>
             <Routes>
-                <Route exact path="/*" element={(<MainPage/>)}/>
+                <Route exact path="/" element={(<MainPage/>)}/>
                 <Route exact path="/admin/*" element={(<DashboardAdmin/>)}/>
+                <Route exact path="/login" element={(<Signup/>)}/>
                 <Route exact path="/dashboard/*" element={(<DashboardUser/>)}/>
             </Routes>
         </BrowserRouter>
